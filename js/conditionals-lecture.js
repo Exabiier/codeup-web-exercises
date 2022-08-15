659f180322a810adb3337aa4d2059daf16cb8c16
Exabiier Bars <Exabiier@yahoo.com>
2022-08-15 16:06:04 -0500

    js file for conditionals lecture.

---
+++ js/conditionals-lecture.js
@@ -0,0 +1,75 @@
+"strict user"
+
+// let flavor = prompt("ice cream do you want")
+//
+// flavor = flavor.toLowerCase()
+
+// if flavor === "chocolate"){
+//     alert("One choclate coming right up");
+// } else if (flavor === "vanilla") {
+//     alert("We got some vanilla 10000!!!!");
+// }
+//
+// else { alert("sorry, we have no flavor");
+//
+// }
+
+// switch (flavor){
+//     case("chocolate"):
+//         alert("One choclate coming right up");
+//         break;
+//     case("vanilla"):
+//         alert("We got some vanilla 10000!!!!");
+//         break;
+//     default:
+//         alert("sorry, we have no flavor");
+// }
+
+// let decision = confirm("Are you sure you want to close this page?")
+// alert(decision);
+
+// if (decision) {
+//     alert("Ok, clsoing the page");
+// } else{
+//     alert("OK, keeping page open.")
+// }
+
+//short hand
+
+// (decision)?alert("closing the page"):alert("keeping page open.");
+
+// let number = prompt("Enter a number")
+
+// function divisableFive(number){
+//     return(number % 5 ===0 $$ number !== null && number !== ""){
+//         alert("That number is divisible by 5")
+//     } else{
+//         alert("That number is not divisible by 5");
+//     }
+// }
+//
+// divisableFive(prompt("Enter a number:");
+//
+// //how does the prompt still work.
+//
+// function divisableFive(number){
+//    alert(number % 5 === 0? "That number is divisible by 5":"That number is not divisible by 5");
+// }
+
+// function alertMVPCustomer(pur){
+//     if (pur > 1000) {
+//         alert("MVP!")
+//     }
+// }
+
+// function isMVP(pur){
+//     return pur >1000;
+// }
+//
+//
+// let customer1pur = 1001;
+//
+// if (isMVP(customer1pur){
+//     alert("MVP!")
+// }
+
